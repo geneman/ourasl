89a95fe8fc5ac2b6a91e3ccfc26ffae3e470f573
Gene Ekster <geneman101@yahoo.com>
2024-10-29 13:54:52 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from oura_ring import OuraClient
+import datetime
 OURA_ACCESS_TOKEN=st.secrets["OURA_ACCESS_TOKEN"]
 client = OuraClient(OURA_ACCESS_TOKEN)
-tot_sleep_all=client.get_daily_sleep()
+date_today = datetime.datetime.today().strftime('%Y-%m-%d')
+tot_sleep_all=client.get_daily_sleep(date_today)
 tot_sleep=tot_sleep_all[0]['contributors']['total_sleep']
 st.write(tot_sleep)
 a=1
