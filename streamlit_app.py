82cb045ef197facfc1942e30eb41042339567635
Gene Ekster <geneman101@yahoo.com>
2024-10-29 00:36:16 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ from oura_ring import OuraClient
 OURA_ACCESS_TOKEN=st.secrets["OURA_ACCESS_TOKEN"]
 client = OuraClient(OURA_ACCESS_TOKEN)
 
-print(client.get_sleep_periods('2024-10-22'))
+st.write(client.get_sleep_periods('2024-10-22'))
