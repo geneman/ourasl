23527545b04e88d2faf92ad5bdc08dbe635cfdc8
Gene Ekster <geneman101@yahoo.com>
2024-10-30 02:33:02 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,11 @@ import datetime
 OURA_ACCESS_TOKEN=st.secrets["OURA_ACCESS_TOKEN"]
 client = OuraClient(OURA_ACCESS_TOKEN)
 date_today = datetime.datetime.today().strftime('%Y-%m-%d')
-tot_sleep_all=client.get_daily_sleep(date_today)
-tot_sleep=tot_sleep_all[0]['contributors']['total_sleep']
-st.write(tot_sleep)
+try:
+    tot_sleep_all=client.get_daily_sleep(date_today)
+    tot_sleep=tot_sleep_all[0]['contributors']['total_sleep']
+    st.write(tot_sleep)
+except:
+    st.write("--")
+
 a=1
